4d34ae57d4e2b284e181930dafab7fd2a4b36037
Elvan Hidayat <elvanhd@gmail.com>
2024-03-30 04:22:56 +0700

    Move GradientContainer to its own file
    - Create gradient_container.dart for the GradientContainer widget
    - Move GradientContainer class from main.dart to new file
    - Import gradient_container.dart in main.dart
    - Maintain "Hello World!" text styling and gradient background
    - Improve project structure and modularity

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice/gradient_container.dart';
 
 void main() {
   runApp(
@@ -8,33 +9,4 @@ void main() {
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
