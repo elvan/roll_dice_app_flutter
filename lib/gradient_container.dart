fd747cdef2b0f952ad778eb3291d9f9d45ad54da
Elvan Hidayat <elvanhd@gmail.com>
2024-03-30 16:44:00 +0700

    Add dice images and update GradientContainer
    - Add dice image assets (dice-1.png to dice-6.png) to project
    - Update pubspec.yaml to include new dice images in assets
    - Remove StyledText widget from GradientContainer
    - Introduce Image.asset to display dice-2.png in GradientContainer
    - Add GradientContainer.purple constructor for default purple gradient
    - Enable dynamic image display within GradientContainer

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -20,8 +23,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello World!'),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
