89c010685ba64f93846ef2b20b4b8f6fe5a103a7
Elvan Hidayat <elvanhd@gmail.com>
2024-03-30 16:14:05 +0700

    Refactor gradient alignments to constants
    - Define startAlignment and endAlignment as constants
    - Replace direct Alignment values in GradientContainer with
      constants for clarity and ease of maintenance
    - Maintain existing gradient effect and widget structure

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
