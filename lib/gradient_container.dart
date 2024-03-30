6aa6379b37a44ddd8622801af2f0efc09fca87df
Elvan Hidayat <elvanhd@gmail.com>
2024-03-30 16:36:44 +0700

    Enhance GradientContainer with dynamic colors
    - Add color1 and color2 parameters to GradientContainer constructor
    - Replace static colors with dynamic color1 and color2 in the gradient
    - Update main.dart to pass custom colors to GradientContainer
    - Allows for flexible color schemes in the gradient background

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,17 +5,17 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
